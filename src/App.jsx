17a10f688a4209ebc0598667ed692217a1f0bca0
░░░░░U░Z░A░I░R░░░░ <uzairshekhani2017@gmail.com>
2024-04-15 23:32:23 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header"
+import Navbar from "./components/Navbar"
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <Navbar/>
+        <Header/>
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='mt-10 flex flex-col gap-5 items-center justify-center text-white'>
+        <h1 className='text-5xl font-bold'>Discover Your Dream Job,Your Search Portal!</h1>
+        <p className='text-xl'>Unlock Your Career Potential, The Premier Job Search Platform!</p>
+    </div>
+  )
+}
+
+export default Header
