afe25c38e4b73366f36256c8f1032240504d070f
░░░░░U░Z░A░I░R░░░░ <uzairshekhani2017@gmail.com>
2024-04-16 23:48:51 +0500

    ui updated

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useEffect } from 'react'
 
 function SearchBar() {
 
-    
+    const [jobCriteria, setjobCriteria] = useEffect({
+        title: "",
+        Location: ""
+
+})
+
   return (
     <div className='flex gap-4 my-10 justify-center px-10'>
         <select className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md'>
