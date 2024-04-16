9692c725e65d4f3b18f9ecd15d42be4dc9a5b9b8
░░░░░U░Z░A░I░R░░░░ <uzairshekhani2017@gmail.com>
2024-04-16 23:52:44 +0500

    ui updated

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -5,13 +5,18 @@ function SearchBar() {
 
     const [jobCriteria, setjobCriteria] = useEffect({
         title: "",
-        Location: ""
+        Location: "",
+        experience: "",
+        type: ""
+    })
 
-})
+    const handleChange = () => {
+
+    }
 
   return (
     <div className='flex gap-4 my-10 justify-center px-10'>
-        <select className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md'>
+        <select onChange={handleChange} name='title'value={jobCriteria.title} className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md'>
             <option value=""  hidden>Job Role</option>
             <option value="ios Developer">ios Developer</option>
             <option value="Frontend Developer">Frontend Developer</option>
@@ -19,19 +24,19 @@ function SearchBar() {
             <option value="ReactJs Developer">ReactJs Developer</option>
             <option value="Laravel Developer">Laravel Developer</option>
         </select>
-        <select className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md'>
+        <select name='title'value={jobCriteria.type} className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md'>
             <option value="" hidden>Job Type</option>
             <option value="ios Developer">Full Time</option>
             <option value="Frontend Developer">Part Time</option>
             <option value="Backend Developer">Contract</option>
         </select>
-        <select className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md'>
+        <select name='title'value={jobCriteria.Location} className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md'>
             <option value=""  hidden>Location</option>
             <option value="ios Developer">Remote</option>
             <option value="Frontend Developer">In-Office</option>
             <option value="Backend Developer">Hybrid</option>
         </select>
-        <select className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md'>
+        <select name='title'value={jobCriteria.experience} className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md'>
             <option value=""  hidden>Experience</option>
             <option value="ios Developer">Fresher</option>
             <option value="Frontend Developer">Junior Level</option>
